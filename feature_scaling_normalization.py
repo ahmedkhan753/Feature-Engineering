582250d1abc07d2bbd31d8510081aa990a9bd76c
ahmedkhan753 <ahmedk32410@gmail.com>
2026-01-12 19:36:12 +0500

    train test split

--- feature_scaling_normalization.py
+++ feature_scaling_normalization.py
@@ -6,5 +6,9 @@ import seaborn as sns
 
 df = pd.read_csv('wine_data.csv', header=None, usecols=[0,1,2])
 df.columns=['Class label', 'Alcohol', 'Malic acid']
-print(df)
-# Normalization is a technique often applied as part of data preparation for machine learning. The goal of normalization is to change the values of numeric columns in the dataset to use a common scale, without distorting differences in the ranges of values or losing information 
+
+# Train test split
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(df.drop('Class label', axis= 1),df['Class label'], test_size=0.3, random_state=0)
+
+print(x_train.shape , x_test.shape)
