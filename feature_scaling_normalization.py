bb61bc584005e528d35f5ebb62eac66283f23381
ahmedkhan753 <ahmedk32410@gmail.com>
2026-01-12 21:49:01 +0500

    min max scaled

--- feature_scaling_normalization.py
+++ feature_scaling_normalization.py
@@ -12,3 +12,24 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(df.drop('Class label', axis= 1),df['Class label'], test_size=0.3, random_state=0)
 
 print(x_train.shape , x_test.shape)
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+
+# Fit the scaler to the train set, it will learn the parameters
+scaler.fit(x_train)
+
+# Transform train and test sets
+x_train_scaled = scaler.transform(x_train)
+x_test_scaled = scaler.transform(x_test)
+
+# Converting numpy array into again in dataframe
+x_train_scaled = pd.DataFrame(x_train_scaled, columns=x_train.columns)
+x_test_scaled = pd.DataFrame(x_test_scaled, columns=x_test.columns)
+
+# old train data without scaling
+print(np.round(x_train.describe(), 1))
+
+# New train data with scaling 
+print(np.round(x_train_scaled.describe(), 1))
